192df9c7c5b20c3362c886a4dd51f883bf004232
srimulyanitarihoran <srimulyanisri449@gmail.com>
2025-10-08 09:48:46 +0700

    configuring the app constant

---
+++ lib/utils/constants.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Constants {
+  static const String baseUrl = 'https://newsapi.org/v2/';
+
+  // Get API KEY from env Variables
+  static String get apiKey => dotenv.env['API_KEY'] ?? '';
+
+  //  list onf endpoints
+  static const String topHeadLines = '/top-headlines';
+  static const String everything = '/everything';
+
+  // list of categories
+  static const List<String> categories = [
+    'general'
+    'technology'
+    'bussiness'
+    'sports'
+    'health'
+    'science'
+    'entertainment',
+  ];
+
+  // countries
+  static const String defaultCountry = 'us';
+
+  // app info
+  static const String appName = 'News App';
+  static const String versionApp = '1.0.0';
+}
