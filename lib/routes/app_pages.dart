1a42fe1a5ad91582c399abfa20cbab467f8e2d37
srimulyanitarihoran <srimulyanisri449@gmail.com>
2025-10-23 10:45:21 +0700

    finishing app routing

---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,31 @@
+// unduk
+// part -> sebuah keyword kalo misal dia adalh induk, bisa berdiri sendiri tanpa bantuan
+import 'package:get/get.dart';
+import 'package:news_app/bindings/home_bindings.dart';
+import 'package:news_app/screens/home_screen.dart';
+import 'package:news_app/screens/news_detail_screen.dart';
+import 'package:news_app/screens/splash_screen.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._(); // function private anonymous
+
+  static const INITIAL = Routes.SPLASH;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.SPLASH,
+      page: () => SplashScreen(),
+    ),
+    GetPage(
+      name: _Paths.HOME,
+      page: () => HomeScreen(),
+      binding: HomeBindings(), // binding buat manggil semua yang ada di controller
+    ),
+    GetPage(
+      name: _Paths.NEWS_DETAIL,
+      page: () => NewsDetailScreen(),
+    )
+  ];
+}
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: constant_identifier_names
+// part of -> gabisa beridir sendiri
+// anak
 
 part of 'app_pages.dart';
 
